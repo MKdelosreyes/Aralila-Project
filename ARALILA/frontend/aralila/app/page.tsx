35c8c96ff7d7aa2381ed046164c3e6939dcbce01
Yana Paragoso <paragosoyana@gmail.com>
2025-11-28 04:07:31 +0800

    landing: new bg and added portal button

--- ARALILA/frontend/aralila/app/page.tsx
+++ ARALILA/frontend/aralila/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import { Button } from "@/components/ui/button"
+import Image from "next/image";
 import Navbar from "@/components/navbar/navbar-home";
 
 export default function Home() {
@@ -10,38 +9,22 @@ export default function Home() {
       <Navbar />
       <main className="text-white">
         <section
-        className="min-h-screen bg-[url('/images/bg/landing-bg.png')] bg-no-repeat bg-center bg-cover flex flex-col items-center justify-center text-center px-4"
-        id="getting-started"
-      >
-        <h1 className="text-5xl font-bold mb-6">Welcome to Aralila</h1>
-        <p className="text-lg mb-8 max-w-xl">
-          Empowering Students and Teachers with Seamless Collaboration.
-        </p>
-        <Link href="/signup">
-          <Button className="text-lg px-8 py-6 bg-white text-purple-700 hover:bg-purple-100 font-semibold rounded-full">
-            Get Started
-          </Button>
-        </Link>
-      </section>
-
-        {/* random section for viewing*/}
-        <section
-          className="min-h-screen bg-gray-900 text-white py-20 px-4"
-          id="testimonials"
+          className="min-h-screen bg-[url('/images/bg/bg-landing-2.png')] bg-no-repeat bg-center bg-cover flex flex-col items-center justify-center text-center px-4"
+          id="getting-started"
         >
-          <h2 className="text-4xl font-bold text-center mb-10">
-            What Our Users Say
-          </h2>
-          <div className="max-w-4xl mx-auto space-y-8">
-            <blockquote className="text-xl italic">
-              “This app changed the way I manage my class!”
-            </blockquote>
-            <blockquote className="text-xl italic">
-              “Super easy to use. My students love it.”
-            </blockquote>
-            <blockquote className="text-xl italic">
-              “THIS IS JUST A FORMATTING VIEW OKAY NOT FINAL”
-            </blockquote>
+          <div className="flex flex-col items-center justify-center translate-y-45">
+            <div className="portal-orbit" aria-hidden="true">
+              <div className="portal-orbiter">
+                <Image 
+                  src="/images/overlays/portal-purple-1.svg" 
+                  alt="portal" 
+                  width={600} 
+                  height={600} 
+                  priority 
+                  className="animate-[spin_10s_linear_infinite_reverse]"
+                />
+              </div>
+            </div>
           </div>
         </section>
       </main>

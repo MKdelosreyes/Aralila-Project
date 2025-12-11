c8ce3406df8ba805965fe15121dc3c2b3c639c72
MKdelosreyes <151009128+MKdelosreyes@users.noreply.github.com>
2025-12-12 04:49:25 +0800

    Story chain images are now visiblee in production - yey

--- ARALILA/frontend/aralila/next.config.ts
+++ ARALILA/frontend/aralila/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com",'dnrwenixinuxocwdlmwk.supabase.co'],
+    domains: ["images.unsplash.com","dnrwenixinuxocwdlmwk.supabase.co"],
     remotePatterns: [
       {
         protocol: "http",

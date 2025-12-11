bd64d6e3b08a34f430f9d44cca2a431160038524
MKdelosreyes <151009128+MKdelosreyes@users.noreply.github.com>
2025-12-12 03:35:49 +0800

    Story chain images visibility

--- ARALILA/backend/backend/urls.py
+++ ARALILA/backend/backend/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path('api/progress/', include('progress.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
